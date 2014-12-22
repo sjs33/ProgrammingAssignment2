39981798605bf5ba66796eb452e8d33db5d81907
Steve Simko <ssimko87@gmail.com>
2014-12-21 20:03:10 -0500

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() must be called first and passed an intertible matrix.
+##    This function then creates a list of functions used to initialize the operation
+## cacheSolve() is the command used to actually call for calculating the inverted matrix.
+##    The argument for the cacheSolve() function must be the list created by the makeCacheMatrix function.
 
-## Write a short comment describing this function
+## By storing the variables outside the local function, these functions avoid recalculating the inversion
+## function if the original matrix has not changed.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -12,4 +25,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
